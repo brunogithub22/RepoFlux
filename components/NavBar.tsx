230c28d69529953108e2172d793c26446db59a8b
bruno <bruno.github.22@gmail.com>
2026-02-04 11:11:02 +0100

    Added dependecis

---
+++ components/NavBar.tsx
@@ -0,0 +1,9 @@
+
+
+export default function NavBar(){
+    return(
+        <nav className="">
+
+        </nav>
+    );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = { 
+  images: { 
+    remotePatterns: [ 
+      { protocol: 'https', 
+        hostname: 'res.cloudinary.com', 
+        pathname: `/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/**`, 
+      } 
+    ], 
+  }, 
 };
 
 export default nextConfig;
