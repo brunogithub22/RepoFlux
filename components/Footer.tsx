ad8617b865f6708096e01d5664768b3c94109b34
Bruno <brunogithub22@gmail.com>
2026-02-04 23:59:35 +0100

    Added icon social

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,17 @@
-
+import Image from "next/image";
 
 export default function Footer(){
     return(
-        <footer className="">
-
+        <footer className="p-4 bg-gray-800 text-white text-center">
+            <div className="py-2">
+                <div className="flex space-x-4 justify-center mb-4">
+                    <Image src="/social/facebook.svg" width={24} height={24} alt="Facebook" loading="eager" />
+                    <Image src="/social/youtube.svg" width={24} height={24} alt="Facebook" loading="eager" />
+                    <Image src="/social/instagram.svg" width={24} height={24} alt="Facebook" loading="eager" />
+                    <Image src="/social/whatsapp.svg" width={24} height={24} alt="Facebook" loading="eager" />
+                </div>
+                <span className="text-sm">&copy; {new Date().getFullYear()} RepoFlux. All rights reserved.</span>
+            </div>
         </footer>
     );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,13 @@
-
+import Image from "next/image";
 
 export default function NavBar(){
     return(
-        <nav className="">
-
+        <nav className="flex items-center p-4 bg-gray-800 text-white">
+            <div className="flex ml-2 items-center">
+                <Image src="icon.svg" width={32} height={32} alt="Logo" loading="eager" className="rounded-full" />
+                <span className="ml-2 font-bold text-lg">RepoFlux</span>
+            </div>
+            
         </nav>
     );
 }
