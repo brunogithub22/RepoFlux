1c8139e88dfaf280e3e550679305cc58ebec959c
Bruno <brunogithub22@gmail.com>
2026-02-06 00:10:29 +0100

    add the login GitHub

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import { users } from './db/schema'
 
-const connectionString: string = process.env.DATABASE_URL!
+const connectionString: string = process.env.POSTGRES_URL!
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(connectionString, { prepare: false })
